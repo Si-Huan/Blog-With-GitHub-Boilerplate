9d53e178c516a69d483566709982938f57e1c133
Si-Huan <49349784+Si-Huan@users.noreply.github.com>
2020-01-05 14:51:31 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -20,7 +20,7 @@ enable_jsdelivr = {
 }
 
 # 站点设置
-site_name = "思还的博客"
+site_name = "SiHuan's Blog"
 site_logo = "${static_prefix}logo.png"
 site_build_date = "2019-12-18T16:51+08:00"
 author = "SiHuan"
