17195bb311dc51c84c68a8ccea9d67d105b3e922
Si-Huan <49349784+Si-Huan@users.noreply.github.com>
2020-01-05 13:44:24 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -9,30 +9,30 @@ build_dir = "../dist/"
 index_page_size = 10
 archives_page_size = 20
 enable_jsdelivr = {
-    "enabled": False,
-    "repo": ""
+    "enabled": True,
+    "repo": "Si-Huan/Blog-With-GitHub-Boilerplate@gh-pages"
 }
 
 # 站点设置
-site_name = "我的个人博客"
+site_name = "思还的博客"
 site_logo = "${static_prefix}logo.png"
 site_build_date = "2019-12-18T16:51+08:00"
-author = "熊猫小A"
-email = "hi@imalan.cn"
-author_homepage = "https://www.imalan.cn"
-description = "只坚持一种正义。我的正义。"
-key_words = ['Maverick', '熊猫小A', 'Galileo', 'blog']
+author = "SiHuan"
+email = "sihuan@sakuya.love"
+author_homepage = "https://www.sakuya.love"
+description = "道阻且长，君子思还。"
+key_words = ['SiHuan', '思还', 'Blog', '博客', 'GitBlog']
 language = 'zh-CN'
 external_links = [
     {
-        "name": "Maverick",
-        "url": "https://github.com/AlanDecode/Maverick",
-        "brief": "🏄‍ Go My Own Way."
+        "name": "SiHuan-Wiki",
+        "url": "https://wiki.sakuya.love",
+        "brief": "我的知识积累。"
     },
     {
-        "name": "三無計劃",
-        "url": "https://www.imalan.cn",
-        "brief": "熊猫小A的主页。"
+        "name": "SiHuan-Sakuya",
+        "url": "https://www.sakuya.love",
+        "brief": "思还的主页。"
     }
 ]
 nav = [
@@ -55,9 +55,9 @@ nav = [
 
 social_links = [
     {
-        "name": "Twitter",
+        "name": "Telegram",
         "url": "https://twitter.com/AlanDecode",
-        "icon": "gi gi-twitter"
+        "icon": "gi gi-telegram"
     },
     {
         "name": "GitHub",
@@ -65,9 +65,9 @@ social_links = [
         "icon": "gi gi-github"
     },
     {
-        "name": "Weibo",
+        "name": "QQ",
         "url": "https://weibo.com/5245109677/",
-        "icon": "gi gi-weibo"
+        "icon": "gi gi-qq"
     }
 ]
 
